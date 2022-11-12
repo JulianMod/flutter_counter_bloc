bcc80f21a605c4d4c9f40e961e275a0a0b642ec1
JuliaMod <julmodlinski@gmail.com>
2022-11-12 19:11:41 +0100

    edit main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_counter_bloc/counter_observer.dart';
+import 'package:flutter_counter_bloc/app.dart';
 
 void main() {
   Bloc.observer = CounterObserver();
